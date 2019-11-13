61552382819200135fd29cf7ce4d7564e99a3d72
Your Name <you@example.com>
2019-11-13 20:20:59 +0100

    Setup and add a public route

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath)) 
 
-app.get("/add", (req, res) => {
+app.get("/create", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"))
 })
 
